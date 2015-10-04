e5e414e92371a7538e8fd6cd7360aa4328faef46
Nikhil Kumar <nikhilkumar516@gmail.com>
2015-10-04 05:02:54 -0400

    fixed import error

--- skindoctorapp.py
+++ skindoctorapp.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 app.config.from_pyfile('SkinDoc.cfg')
